a98423c13b4a9d30183bbe4d5fe396ed6dd6cccd
IradLevy <levyirad@gmail.com>
2024-04-23 23:07:06 +0300

    \sdf

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import photos from './photos.json'
 import { useState } from 'react'
 import './GalleryStyle.css'
--- src/components/managerSummary/ManagerSummary.js
+++ src/components/managerSummary/ManagerSummary.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import './ManagerStyle.css'
 import summary from './managerSummary.json'
